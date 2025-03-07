c722443fd212a6770038ab789bfa959a5825e126
Jose Maria Rapariz <josemariarapariz@gmail.com>
2025-03-07 12:41:43 +0100

    Loop option enabled after md breakpoint

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,39 @@
 /*=============== SWIPER JS ===============*/
-let swiperCards = new Swiper(".card__content", {
-  loop: false,
-  spaceBetween: 32,
-  grabCursor: true,
+function initSwiper() {
+  let isLoopEnabled = window.innerWidth >= 768; // Loop enabled for screens 768px and larger
 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
+  let swiperCards = new Swiper(".card__content", {
+    loop: isLoopEnabled,
+    spaceBetween: 32,
+    grabCursor: true,
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
     },
-    968: {
-      slidesPerView: 3,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-  },
+
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      968: {
+        slidesPerView: 3,
+      },
+    },
+  });
+}
+
+// Initialize Swiper on page load
+initSwiper();
+
+// Reinitialize Swiper when resizing the window
+window.addEventListener("resize", function () {
+  document.querySelector(".card__content").swiper.destroy(true, true);
+  initSwiper();
 });
