0430f030d01e317f241db1fbe8f4f60efb9deb5d
Jose Maria Rapariz <josemariarapariz@gmail.com>
2025-03-18 11:22:28 +0100

    Remove swiper loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,26 @@
 /*=============== SWIPER JS ===============*/
-function initSwiper() {
-  let isLoopEnabled = window.innerWidth >= 768; // Loop enabled for screens 768px and larger
+let swiperCards = new Swiper(".card__content", {
+  loop: false,
+  spaceBetween: 32,
+  grabCursor: true,
 
-  let swiperCards = new Swiper(".card__content", {
-    loop: isLoopEnabled,
-    spaceBetween: 32,
-    grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
 
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
     },
-
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    968: {
+      slidesPerView: 3,
     },
-
-    breakpoints: {
-      768: {
-        slidesPerView: 2,
-      },
-      968: {
-        slidesPerView: 3,
-      },
-    },
-  });
-}
-
-// Initialize Swiper on page load
-initSwiper();
-
-// Reinitialize Swiper when resizing the window
-window.addEventListener("resize", function () {
-  document.querySelector(".card__content").swiper.destroy(true, true);
-  initSwiper();
+  },
 });
